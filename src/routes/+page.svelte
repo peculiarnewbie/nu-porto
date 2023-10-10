ead12819ef4b5e01248644e369962a0418b44758
peculiarnewbie <32804415+peculiarnewbie@users.noreply.github.com>
2023-10-10 15:38:40 +0700

    switch to sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import "../app.css";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class=" flex flex-col p-12">
+	<h1 class="text-9xl">come on</h1>
+</div>

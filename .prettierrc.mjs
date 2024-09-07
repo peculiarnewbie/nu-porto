5c778f6c72587754d9d303b2b8631dc7366cc2e0
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2024-09-07 13:55:42 +0700

    home layout

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  useTabs: true,
+  printWidth: 100,
+  tabWidth: 4,
+};
--- src/lib/stores.js
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store"
-
-export const mousePos = writable([0, 0])
-
-export const sceneSize = writable(1)
